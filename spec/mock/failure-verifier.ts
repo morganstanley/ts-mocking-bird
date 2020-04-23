d45bcc54a8a071a463ba978030555b8a83eafc09
unknown <10414642+Roaders@users.noreply.github.com>
2020-04-23 10:01:28 +0100

    Fix: add jest message tests

--- spec/mock/failure-verifier.ts
+++ spec/mock/failure-verifier.ts
@@ -11,3 +11,15 @@ export function verifyFailure(
     expect(result.pass).toBeFalsy();
     expect(result.message).toEqual(message);
 }
+
+export function verifyJestFailure(
+    verifier: IFunctionVerifier<any, any, any>,
+    matcher: jasmine.CustomMatcher,
+    message: string,
+    ...params: any[]
+) {
+    const result = (matcher.compare(verifier, ...params) as unknown) as jest.CustomMatcherResult;
+
+    expect(result.pass).toBeFalsy();
+    expect(result.message()).toEqual(message);
+}
