6bd60f6c64417d14b67d58a369a089eed595b007
Giles Roandight <10414642+Roaders@users.noreply.github.com>
2026-03-09 13:49:41 +0000

    convert to lodash-es

--- main/mock/parameterMatchers.ts
+++ main/mock/parameterMatchers.ts
@@ -1,4 +1,4 @@
-import { isEqual } from 'lodash';
+import { isEqual } from 'lodash-es';
 import { IParameterMatcher } from './contracts.js';
 
 export function isParameterMatcher(matcher: unknown): matcher is IParameterMatcher<any> {
