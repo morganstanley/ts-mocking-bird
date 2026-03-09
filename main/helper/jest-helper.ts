89e3ce413d565e6dd7e8363c3dc3eae3c4303e62
Giles Roandight <10414642+Roaders@users.noreply.github.com>
2026-03-09 09:48:19 +0000

    Remove module replacement functionality

--- main/helper/jest-helper.ts
+++ main/helper/jest-helper.ts
@@ -1,28 +1,7 @@
-import { proxyModule, WrappedModule } from './module-helper';
-
 /**
- * Wraps an existing module and prepares it for use in jest.mock
- *
- * jest.mock('../../relativeImportPath', () =>
- *      require('@morgan-stanley/ts-mocking-bird').proxyJestModule(
- *          require.resolve('../../relativeImportPath'),
- *      ),
- * );
- *
- * @param absolutePath
+ * Determines if we're currently running in Jest
+ * @returns
  */
-export function proxyJestModule(absolutePath: string): WrappedModule {
-    if (typeof jest !== 'object' || typeof jest.requireActual !== 'function') {
-        throw new Error(`proxyJestModule can only be used when jest is available`);
-    }
-    const realModule = jest.requireActual(absolutePath);
-    const wrappedModule = proxyModule(realModule);
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    const brImport = jest.genMockFromModule<object>(absolutePath);
-
-    return { ...brImport, ...wrappedModule };
-}
-
 export function runningInJest(): boolean {
     try {
         return typeof (expect as any).extend === 'function';
